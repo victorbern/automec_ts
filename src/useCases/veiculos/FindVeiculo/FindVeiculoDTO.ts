a579e19ffc165f1778a962c053e5c7dad6ce433e
victorbern <victorbernardo96@gmail.com>
2023-07-24 17:33:58 -0300

    Adicionando a response FindVeiculoDTO no UseCase de FindVeiculo

--- src/useCases/veiculos/FindVeiculo/FindVeiculoDTO.ts
+++ src/useCases/veiculos/FindVeiculo/FindVeiculoDTO.ts
@@ -1,3 +1,15 @@
 export interface IFindVeiculoRequestDTO {
     placaVeiculo: string;
 }
+
+export interface IFindVeiculoResponseDTO {
+    placaVeiculo: string;
+    marca: string;
+    modelo: string;
+    ano: number;
+    capacidadeOleo: number;
+    cor: string;
+    veiculo_idCliente: number;
+    nomeCliente: string;
+    celularCliente: string;
+}
--- src/useCases/veiculos/FindVeiculo/FindVeiculoUC.ts
+++ src/useCases/veiculos/FindVeiculo/FindVeiculoUC.ts
@@ -1,14 +1,27 @@
 import { IVeiculosRepository } from "../../../repositories/IVeiculosRepository";
-import { IFindVeiculoRequestDTO } from "./FindVeiculoDTO";
+import { findClienteUC } from "../../FindCliente";
+import { IFindVeiculoRequestDTO, IFindVeiculoResponseDTO } from "./FindVeiculoDTO";
 
 export class FindVeiculoUC {
     constructor(
         private veiculosRepository: IVeiculosRepository,
     ) {}
 
-    async execute(data: IFindVeiculoRequestDTO) {
+    async execute(data: IFindVeiculoRequestDTO): Promise<IFindVeiculoResponseDTO> {
         try {
-            const veiculo = await this.veiculosRepository.findByPlacaVeiculo(data.placaVeiculo)
+            const result = await this.veiculosRepository.findByPlacaVeiculo(data.placaVeiculo)
+            const cliente = await findClienteUC.execute({idCliente: result.idCliente})
+            const veiculo = {
+                placaVeiculo: result.placaVeiculo,
+                marca: result.marca,
+                modelo: result.modelo,
+                ano: result.ano,
+                capacidadeOleo: result.capacidadeOleo,
+                cor: result.cor,
+                veiculo_idCliente: result.idCliente,
+                nomeCliente: cliente.nomeCliente,
+                celularCliente: cliente.celularCliente,
+            };
             return veiculo;
             
         } catch (error) {
