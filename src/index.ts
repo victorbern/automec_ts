bd45cb8e35c9fef151dc1396abdc255a6961d7a5
victorbern <victorbernardo96@gmail.com>
2023-10-13 19:35:01 -0300

    Instalação do cors e do qs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { router } from './routes';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(router);
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 export { app }
